72e27cd1eae2c19f76fbaf6bd3ecc3dc287bdd84
v_huangyn <v_huangyn@digitalgd.com.cn>
2024-04-06 20:37:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <router-view/>
+    <tab-bar/>
+  </div>
+</template>
+
+<script setup>
+
+import TabBar from "@/components/toolbar/tab-bar.vue"
+import { useRoute } from "vue-router";
+const router = useRoute()
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/home/home.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div class="home">
+        <home-nav-bar/>
+        <div class="banner">
+            <img src="@/assets/img/home/banner.webp"/>
+        </div>
+        <home-search-box />
+    </div>
+  </template>
+  <script setup>
+  import homeNavBar from './cpns/home-nav-bar.vue';
+  import homeSearchBox from './cpns/home-search-box.vue';
+  import useHomeStore from '@/stores/modules/home';
+// 尽量不要在小组件发送网络请求，发送网络请求
+const homeStore = useHomeStore()
+homeStore.fetchHotSuggestData()
+  </script>
+  
+  <style lang="less">
+    .banner{
+        img{
+            width: 100%;
+        }
+    }
+  </style>
+  
